7d1a17ff83f34704e6fa477ee7bc4f51417dcc6d
gaborszenesi <gaborszenesi@gmail.com>
2024-07-17 14:41:56 +0100

    fix

--- update_and_run.sh
+++ update_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker stop $(sudo docker ps -q)
 git pull
+sudo docker stop $(sudo docker ps -q)
 docker build -t fooocus-api-pb .
 sudo docker run --network host --gpus all -p 8000:8000 fooocus-api-pb
