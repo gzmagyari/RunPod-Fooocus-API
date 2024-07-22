117007bcdb399cf45ad7c94131c5d356e239171e
gaborszenesi <gaborszenesi@gmail.com>
2024-07-22 17:24:05 +0100

    multi

--- src/start.sh
+++ src/start.sh
@@ -3,8 +3,16 @@ echo "Worker Initiated"
 
 echo "Starting Fooocus API"
 cd /workspace
-python launch.py --disable-in-browser --always-gpu --disable-offload-from-vram --listen 0.0.0.0 --port 8887 & # You can add more Fooocus flags here to optimize performance for your workers, see https://github.com/lllyasviel/Fooocus?tab=readme-ov-file#all-cmd-flags
-python launch.py --disable-in-browser --always-gpu --disable-offload-from-vram --listen 0.0.0.0 --port 4447 &
+
+mkdir -p /temp/fooocus_output1
+mkdir -p /temp/fooocus_temp1
+mkdir -p /temp/cache1
+mkdir -p /temp/fooocus_output2
+mkdir -p /temp/fooocus_temp2
+mkdir -p /temp/cache2
+
+python launch.py --disable-in-browser --always-gpu --disable-offload-from-vram --listen 0.0.0.0 --port 8887 --output-path /temp/fooocus_output1 --temp-path /temp/fooocus_temp1 --cache-path /temp/cache1 & # You can add more Fooocus flags here to optimize performance for your workers, see https://github.com/lllyasviel/Fooocus?tab=readme-ov-file#all-cmd-flags
+python launch.py --disable-in-browser --always-gpu --disable-offload-from-vram --listen 0.0.0.0 --port 4447 --output-path /temp/fooocus_output2 --temp-path /temp/fooocus_temp2 --cache-path /temp/cache2 &
 
 echo "Starting RunPod Handler"
 cd /
