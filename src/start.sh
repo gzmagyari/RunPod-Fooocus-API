2a1454ac18e7bda96ddba087b53be4ca45712866
gaborszenesi <gaborszenesi@gmail.com>
2024-07-25 21:07:14 +0100

    updates

--- src/start.sh
+++ src/start.sh
@@ -3,8 +3,8 @@ echo "Worker Initiated"
 
 echo "Starting Fooocus API"
 cd /workspace
-python launch.py --disable-in-browser --always-gpu --disable-offload-from-vram --port 8887 & # You can add more Fooocus flags here to optimize performance for your workers, see https://github.com/lllyasviel/Fooocus?tab=readme-ov-file#all-cmd-flags
+#python launch.py --disable-in-browser --always-gpu --disable-offload-from-vram --port 8887 & # You can add more Fooocus flags here to optimize performance for your workers, see https://github.com/lllyasviel/Fooocus?tab=readme-ov-file#all-cmd-flags
 
 echo "Starting RunPod Handler"
 cd /
-python -u /handler.py
+python -u /multi_handler.py
